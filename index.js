046100900a50bc464009b527eebb09a6c84594a7
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-19 06:56:53 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = (num1, num2) => {
+    console.log(`Divide ${num1}`);
+    console.log(`Divide ${num2}`);
+    return 2000/100;
+}
+
+
+const square = x => x * x;
+square (2);
+
+
+const add = (num1, num2) => {
+    console.log(`Adding ${num1}`);
+    console.log(`Adding ${num2}`);
+    return(num1 + num2);
+}
+add(3,4);
